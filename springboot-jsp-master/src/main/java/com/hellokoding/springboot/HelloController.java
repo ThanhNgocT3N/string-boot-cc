5184769ec674a16573178d0d40c1b85ea62624dc
ThanhNgocT3N <14110132@student.hcmute.edu.vn>
2017-05-05 07:53:49 +0700

    dtdm
    dtdm

---
+++ springboot-jsp-master/src/main/java/com/hellokoding/springboot/HelloController.java
@@ -0,0 +1,26 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+    @RequestMapping("/edit")
+    public String edit(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "ckeditor";
+    }
+		
+    @RequestMapping("/ic")
+    public String ICSSE(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "test";
+    }
+}
